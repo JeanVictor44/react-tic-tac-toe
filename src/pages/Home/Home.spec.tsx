84451430ce138fc87944cd56d680c719867b3bd5
JeanVictor44 <jeanvictorgithubprojects@gmail.com>
2023-08-13 12:35:37 -0300

    fix(pages/Home): remove test to verify logo

--- src/pages/Home/Home.spec.tsx
+++ src/pages/Home/Home.spec.tsx
@@ -1,11 +1,6 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
-import { HomePage } from "./index";
+import { describe } from "vitest";
 
 describe("Home Page", () => {
-  test("Should be abble to see the logo of the page", () => {
-    const { getByTestId } = render(<HomePage />);
-    expect(getByTestId("logo")).toBeInTheDocument();
-  });
+
 });
