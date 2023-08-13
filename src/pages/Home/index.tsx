8049c4afc4dd6489c053d1e6296d15c1a34af1db
JeanVictor44 <jeanvictorgithubprojects@gmail.com>
2023-08-13 16:31:40 -0300

    feat(pages/Home): create home page without animation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,49 @@
+import "./styles.scss";
+import logo from "../../assets/logo.svg";
+import xBlack from "../../assets/icons/icon-x-black.svg";
+import oGray from "../../assets/icons/icon-o-grey.svg";
+import oBlack from "../../assets/icons/icon-o-black.svg";
+import xGray from "../../assets/icons/icon-x-grey.svg";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Marks } from "../../enums/marks";
+
 export const HomePage = () => {
+  const { t } = useTranslation();
+  const [markPlayerOne, setMarkPlayerOne] = useState<Marks>(Marks.x);
+
+  function toggleToX() {
+    setMarkPlayerOne(Marks.x);
+  }
+
+  function toggleToO() {
+    setMarkPlayerOne(Marks.o);
+  }
+
   return (
-    <h1>Home page</h1>
-  )
-}
+    <div className="home-container">
+      <div className="logo-container">
+        <img src={logo} alt="Logo com X e O"/>
+      </div>
+
+      <div className="choose-mark-box">
+        <p className="choose-mark-box__title">{t("Pick player mark")}</p>
+        <div className="choose-mark-box__buttons-box">
+          <button onClick={toggleToX} id="button-x" className={`${markPlayerOne === Marks.x && 'mark_choosed'}`}>
+            <img src={markPlayerOne === Marks.x ? xBlack : xGray} />
+          </button>
+          <button onClick={toggleToO} id="button-o" className={`${markPlayerOne === Marks.o && 'mark_choosed'}`}>
+            <img src={markPlayerOne === Marks.o ? oBlack : oGray} />
+          </button>
+          <div className="button-background"></div>
+        </div>
+        <p className="choose-mark-box__reminder">{t("X first")}</p>
+      </div>
+
+      <div className="actions-buttons-container" >
+        <button id="button-vs-cpu">{t("New game cpu")}</button>
+        <button id="button-vs-player">{t("New game player")}</button>
+      </div>
+    </div>
+  );
+};
